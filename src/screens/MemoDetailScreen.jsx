c6638e6d5dbfd7870700726290365b91667d95c2
tokutaro <22991683+tokutaro@users.noreply.github.com>
2023-09-29 17:24:11 +0900

    メモ詳細画面作った

---
+++ src/screens/MemoDetailScreen.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoDetailScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2000年11月19日 23:00</Text>
+      </View>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoText}>
+          買い物リスト
+          ああああああああああああああああああああああ{'\n'}
+          いいいいいいいいいいいいいいいいいいいいいいいいいいいい
+          うううううううううううううううううううううううううううううううううううううううう
+        </Text>
+      </ScrollView>
+      <CircleButton style={{ top: 160, buttom: 'auto' }}>+</CircleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#DDDDDD',
+  },
+  memoHeader: {
+    height: 96,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoTitle: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  memoDate:{
+    color: '#FFFFFF',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
